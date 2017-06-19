d0098e12c3190ec9dc45604ddaa9c239a552f443
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-19 19:25:54 +0900

    Generate Pin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619054849) do
+ActiveRecord::Schema.define(version: 20170619102150) do
 
   create_table "boards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                     null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170619054849) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.index ["user_id"], name: "index_boards_on_user_id", using: :btree
+  end
+
+  create_table "pins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title",                     null: false
+    t.string   "image",                     null: false
+    t.integer  "user_id",                   null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["user_id"], name: "index_pins_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
