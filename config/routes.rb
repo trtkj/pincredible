d09bb7fa2d101bb2dc15d02f34680956ff19bb36
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-19 18:38:33 +0900

    Define Pins actions and configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root  "pins#index"
   devise_for :users
-  resources :users, only: [:show, :update] do
-    resources :boards, only: [:show, :create, :update]
-  end
+  resources :users, only: [:show, :update]
+  resources :boards, only: [:show, :create, :update]
+  resources :pins, only: [:index, :create, :update, :show, :destroy]
 end
