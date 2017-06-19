b903d7f64a5d36a161b41f69669fd3db01031204
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-19 17:09:46 +0900

    Generate UsersController and modify sign-up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root  "pins#index"
   devise_for :users
-  resources :boards, only: [:show, :create, :update]
+  resources :users, only: [:show, :update] do
+    resources :boards, only: [:show, :create, :update]
+  end
 end
