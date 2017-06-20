be2cd394b4d308a9042426574378d1b78df99f77
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-20 21:10:20 +0900

    Configure associations for pin and board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,4 @@
 class Board < ApplicationRecord
+  belongs_to :users
+  has_many :pins, through: :boards_pins
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,4 @@
 class Pin < ApplicationRecord
+  belongs_to :users
+  has_many :boards, through: :boards_pins
 end
