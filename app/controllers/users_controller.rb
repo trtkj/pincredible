0227fefb1237bec84609732ba423fb9a5334afd6
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-20 20:58:36 +0900

    Configure routes and actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+
+  private
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to :root unless @user == current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :image, :about)
   end
 end
