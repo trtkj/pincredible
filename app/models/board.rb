2d152e7af5cfccf5199f9827ca6100b38134c41b
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-21 12:08:57 +0900

    Configure validations for models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ApplicationRecord
   belongs_to :users
   has_many :pins, through: :board_pins
+  validates :title, presence: true
+  validates :user_id, presence: true
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ApplicationRecord
   belongs_to :users
   has_many :boards, through: :board_pins
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :user_id, presence: true
 end
