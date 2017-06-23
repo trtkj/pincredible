025e281755f224362ea5fc4c1739971e329794de
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-23 11:36:27 +0900

    Configure avatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:nickname, :email, :image, :about)
+    params.require(:user).permit(:nickname, :email, :avatar, :about)
   end
 end
