e85befb8a877b9320300eda924725c2080496c95
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-26 12:21:18 +0900

    Configure associations for board_pins

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,7 @@
 class Board < ApplicationRecord
   belongs_to :user
   has_many :pins, through: :board_pins
+  has_many :board_pins
   validates :title, presence: true
   validates :user_id, presence: true
 end
