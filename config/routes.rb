0f397fc85301a362f73605c919e6de0b5aa00d2b
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-29 15:57:50 +0900

    Configure routes for user#index and board#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root  "pins#index"
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
-  resources :boards, only: [:show, :create, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :boards, only: [:index, :show, :create, :update]
   resources :pins, only: [:index, :create, :update, :show, :destroy]
   resources :board_pins, only: [:create, :destroy]
 end
