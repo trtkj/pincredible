50e24919aa12b9a02af652cea8a0c41055af900e
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-07-03 18:28:44 +0900

    Configure messages and destroy action for board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,7 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :update]
   def index
-    respond_to do |format|
-      format.json {
-        @search_boards = Board.where('title LIKE(?)', "%#{params[:content]}%").limit(5)
-      }
-    end
+    @search_boards = Board.where('title LIKE(?)', "%#{params[:content]}%").limit(5)
   end
 
   def show
@@ -15,15 +11,27 @@ class BoardsController < ApplicationController
   def create
     @board = Board.new(board_params)
     if @board.save
-      redirect_to @board
+      redirect_to(@board, notice: "ボードを作成しました")
     else
+      redirect_to(@board.user, flash: { errors: @board.errors.full_messages })
     end
   end
 
   def update
+    @board = Board.find(params[:id])
     if @board.update(board_params)
       redirect_to @board
     else
+      redirect_to(@board, flash: { errors: @board.errors.full_messages })
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    if @board.destroy
+      redirect_to(user_path(current_user), notice: "「" + @board.title + "」を削除しました")
+    else
+      redirect_to(user_path(current_user), flash:{ errors: @board_pin.errors.full_messages })
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root  "pins#index"
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
-  resources :boards, only: [:index, :show, :create, :update]
+  resources :boards, only: [:index, :show, :create, :update, :destroy]
   resources :pins, only: [:index, :create, :update, :show, :destroy]
   resources :board_pins, only: [:create, :destroy]
 end
