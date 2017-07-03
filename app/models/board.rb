f3997e7ba35b2e17d506551ea8d9fb434b1e97c1
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-07-03 18:26:33 +0900

    Configure validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,5 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :pins, through: :board_pins
   has_many :board_pins
-  validates :title, presence: true
-  validates :user_id, presence: true
+  validates :title, :user_id, presence: true
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,8 +2,6 @@ class Pin < ApplicationRecord
   belongs_to :user
   has_many :boards, through: :board_pins
   has_many :board_pins
-  validates :title, presence: true
-  validates :image, presence: true
-  validates :user_id, presence: true
+  validates :title, :image, :user_id, presence: true
   mount_uploader :image, ImageUploader
 end
