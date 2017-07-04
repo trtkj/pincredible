9ed6e28289a19780e0f526010adf0a21a04bc71d
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-07-04 19:22:33 +0900

    Define a method for extracting tags

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,4 +6,17 @@ class Pin < ApplicationRecord
   has_many :pin_tags
   validates :title, :image, :user_id, presence: true
   mount_uploader :image, ImageUploader
+
+  def extract_tags
+    oldPinTags = PinTag.where(pin_id: self.id)
+    oldPinTags.each do |pintag|
+      pintag.destroy
+    end
+    tags = self.description.scan(/#([^(\s|#)]+)\s?/)
+    tags.each do |tag|
+      newTag = Tag.where(name: tag).first_or_initialize
+      newTag.save
+      PinTag.create(tag_id: newTag.id, pin_id: self.id)
+    end
+  end
 end
