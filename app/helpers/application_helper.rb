e83f60700940932d546993a4f2ded576a5c6a396
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-07-04 19:26:53 +0900

    Define tag2link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def my_page?
     user_signed_in? && current_user == @user
   end
+
+  def tag2link(text)
+    newText = String.new(text)
+    text.scan(/#([^(\s|#)]+)\s?/){ |matched|
+      tag = Tag.find_by(name: matched)
+      newText.gsub!("#" + matched[0], "<a class = 'tag-link' href = '/tags/#{tag.id}'>##{tag.name}</a> ")
+    }
+    return newText;
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ApplicationRecord
   has_many :pins, through: :pin_tags
   has_many :pin_tags
   validates :name, presence: true
+
 end
